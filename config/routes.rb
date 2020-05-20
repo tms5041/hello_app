d70eb1392b45dd3debc5ac8018f931c757fe795f
tms5041 <tms5041@gmail.com>
2020-05-20 14:53:39 +0000

    added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  get 'static_pages/home'
+  get 'static_pages/jobs'
+  get 'static_pages/living'
+  get 'static_pages/fun'
+  root 'static_pages#home'
 end
